90a13c9d823d705f0f9045d50fb0616e8cd5afdb
Freaking-Bob <lyonico645@gmail.com>
2025-12-05 15:46:51 -0600

    data fetching ver1

--- Data_cleaning/main.py
+++ Data_cleaning/main.py
@@ -3,11 +3,12 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from Data_collection.fetch_raw_data import fetch_and_save_raw_data 
 from Data_cleaning.clean_data import clean_raw_data 
 
-RAW_DATA_PATH = "Rawdata/games_raw.json"
-CLEAN_DATA_PATH = "data/processed/games_clean.csv"
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+RAW_DATA_PATH = os.path.join(BASE_DIR, "Data_collection", "Rawdata", "games_filtered.json")
+CLEAN_DATA_PATH = os.path.join(BASE_DIR, "Data_cleaning", "data", "processed", "games_clean.csv")
 
 if __name__ == "__main__":
     
@@ -15,10 +16,7 @@ if __name__ == "__main__":
 
     os.makedirs(os.path.dirname(RAW_DATA_PATH), exist_ok=True)
 
-    print("--- STEP 1: Fetching Raw Data ---")
-    fetch_and_save_raw_data(RAW_DATA_PATH)
-
-    print("\n--- STEP 2: Cleaning and Feature Engineering ---")
+    print("\n--- Cleaning and Feature Engineering ---")
     clean_raw_data(RAW_DATA_PATH, CLEAN_DATA_PATH)
     
     print("\n--- Complete ---")
